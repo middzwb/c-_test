de51d6d0d5174e7649e6ace0448fb6d8c06bb0da
middzwb <zwb843536229@gmail.com>
2020-12-14 20:10:54 +0800

    update test

--- src/test.cc
+++ src/test.cc
@@ -2,6 +2,7 @@
 #include <memory>
 #include <sstream>
 #include <thread>
+#include <cassert>
 
 using namespace std;
 
@@ -14,23 +15,59 @@ shared_ptr<ostringstream> make_request(const shared_ptr<istringstream>& ss) {
     }
     return nullptr;
 }
+void test_make_request() {
+    auto ss{make_shared<istringstream>("test sstringstream")};
+    auto ret = make_request(ss);
+    if (ret) {
+        cout << ret->str() << endl;
+    }
+    auto ret2 = make_request(nullptr);
+    assert(!ret2);
+}
 
-void main_test() {
+template<typename T>
+class Response {
+public:
+    Response()
+        :out_(make_shared<T>())
+    {}
+    ~Response() = default;
+    Response(shared_ptr<T>&& r)
+        :out_(move(r))
     {
-        auto ss{make_shared<istringstream>("test sstringstream")};
-        auto ret = make_request(ss);
-        if (ret) {
-            cout << ret->str() << endl;
-        }
-        auto ret2 = make_request(nullptr);
-        if (ret2) {
-            cout << ret2->str() << endl;
-        }
-        else {
-            cout << "no out stringstream" << endl;
-        }
+        cout << "move constructor" << endl;
+    }
+    const T& out() const { cout << "const" << endl; return *out_; }
+    T& out() { cout << "no const" << endl; return *out_; }
+public:
+    shared_ptr<T> out_;
+};
+void test_response() {
+    class Temp {
+    public:
+        Temp() = default;
+        ~Temp() { cout << "temp destructor" << endl; }
+    };
+    {
+        cout << "Test move ..." << endl;
+        Response<ostringstream> resp;
+        resp.out().str("hello");
+        cout << resp.out().str() << endl;
     }
 
+    {
+        auto s{make_shared<ostringstream>()};
+        s->str("move");
+        assert(s.use_count() == 1);
+        Response<ostringstream> mr(move(s));
+        assert(s.use_count() == 0 && mr.out_.use_count() == 1);
+    }
+
+}
+
+void main_test() {
+    test_make_request();
+    test_response();
 }
 
 int main()
